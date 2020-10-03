92a308fbec2ef4d642d508d527bb668cf19b7e1f
abhick <ezioabhick@gmail.com>
2020-10-03 11:12:10 +0545

    aws commit

---
+++ build/precache-manifest.d64e6158bf4abdee476081046c589990.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d71100ff72f96195858515d5e15f960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac81d868a449909be7b4",
+    "url": "/static/css/main.d2c1bbed.chunk.css"
+  },
+  {
+    "revision": "b8f5be814a9610e8c8da",
+    "url": "/static/js/2.b2de48c0.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b2de48c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac81d868a449909be7b4",
+    "url": "/static/js/main.840420ee.chunk.js"
+  },
+  {
+    "revision": "a4382debbea0b6828cac",
+    "url": "/static/js/runtime-main.1c4dadcd.js"
+  }
+]);
